f309e488019c024471122d7815bc53bc8e226f94
Rajendra Pardeshi <rajendrasp79@outlook.com>
2024-01-23 19:23:43 +0530

    initial changes

---
+++ GNearbyLib/GNearbyLib.cpp
@@ -0,0 +1,12 @@
+// GNearbyLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <iostream>
+
+// TODO: This is an example of a library function
+void fnGNearbyLib()
+{
+	std::cout << "Inside lib" << std::endl;
+}
